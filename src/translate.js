89b8a7d133b69786026f33d499a80315bcb93853
Gil Tayar <gil@tayar.org>
2017-10-30 18:26:47 +0200

    refactored translate to remove built-in pre and postfix

--- src/translate.js
+++ src/translate.js
@@ -2,7 +2,7 @@
 const {JSDOM} = require('jsdom')
 const jQuery = require('jquery-slim')
 
-function translate(html, utmContent) {
+function translate(prefix, postfix, html) {
   const {window} = new JSDOM(html)
   const {document} = window
 
@@ -13,37 +13,8 @@ function translate(html, utmContent) {
   $('*[style]').removeAttr('style')
   $('*[class]').removeAttr('class')
 
-  $('body').append(`<!-- postfix -->
-  <style>
-  h2,
-  h3,
-  h4,
-  h5 {
-    font-weight: bold
-  }
-  </style>
-  <p>
-  <a href="https://applitools.com/landing/free-account-qa/?utm_source=Blog&amp;utm_medium=banner&amp;utm_content=${utmContent}"
-    target="_blank">
-    <img src="https://dl.dropboxusercontent.com/s/0llyxvhfyuj3ich/Automation_expert_coverage.png" style="min-width:600px!important;margin-left:-20px!important;">
-  </a>
-</p>
-
-<p>
-  <u>About the Author:</u>
-  <br> 30 years of experience have not dulled the fascination Gil Tayar has with software development. From the olden days of
-  DOS, to the contemporary world of Software Testing, Gil was, is, and always will be, a software developer. He has in the
-  past co-founded WebCollage, survived the bubble collapse of 2000, and worked on various big cloudy projects at Wix.
-  <br> His current passion is figuring out how to test software, a passion which he has turned into his main job as Evangelist
-  and Senior Architect at Applitools. He has religiously tested all his software, from the early days as a junior software
-  developer to the current days at Applitools, where he develops tests for software that tests software, which is almost
-  one meta layer too many for him.
-  <br> In his private life, he is a dad to two lovely kids (and a cat), an avid reader of Science Fiction, (he counts Samuel
-  Delany, Robert Silverberg, and Robert Heinlein as favorites) and a passionate film buff. (Stanley Kubrick, Lars Von Trier,
-  David Cronenberg, anybody?)
-  <br> Unfortunately for him, he hasn’t really answered the big question of his life - he still doesn't know whether static languages
-  or dynamic languages are best.
-</p>`)
+  prefix && $('body').prepend(prefix)
+  postfix && $('body').append(postfix)
 
   return document.documentElement.innerHTML
 }
--- test/unit/translate.unit.test.js
+++ test/unit/translate.unit.test.js
@@ -5,53 +5,46 @@ const {expect} = require('chai')
 const translate = require('../../src/translate')
 
 describe('translate (unit)', function() {
+  const simpleTranslate = html => removePreambles(translate('', '', html))
+
   it('should remove style tags', () => {
     const input = '<p>lalala</p><style>bababab</style><p>foo</p>'
     const expected = '<p>lalala</p><p>foo</p>'
 
-    expect(removePreambles(translate(input))).to.equal(expected)
+    expect(simpleTranslate(input)).to.equal(expected)
   })
 
   it('should remove all style attributes', () => {
     const input = '<p style="color:red">lalala</p><p><span style="color:blue">f</span>oo</p>'
     const expected = '<p>lalala</p><p><span>f</span>oo</p>'
 
-    expect(removePreambles(translate(input))).to.equal(expected)
+    expect(simpleTranslate(input)).to.equal(expected)
   })
 
   it('should remove all class attributes', () => {
     const input = '<p class="foo bar">lalala</p><p><span class="foo zar">f</span>oo</p>'
     const expected = '<p>lalala</p><p><span>f</span>oo</p>'
 
-    expect(removePreambles(translate(input))).to.equal(expected)
+    expect(simpleTranslate(input)).to.equal(expected)
   })
 
   it('should remove all figcaptions', () => {
     const input = '<figure><img src="asdfasdf"><figcaption>Credit!</figcaption></figure>'
     const expected = '<figure><img src="asdfasdf"></figure>'
 
-    expect(removePreambles(translate(input))).to.equal(expected)
+    expect(simpleTranslate(input)).to.equal(expected)
   })
 
-  it('should add a preamble', () => {
-    const input = '<p>foo</p>'
-    const expectedToInclude1 = '<style>'
-    const expectedToInclude2 = '</style>'
+  it('should add a prefix and postfix', () => {
+    const input = '<body>x<p>foo</p>y</body>'
+    const prefix = '<div>prefix</div>'
+    const postfix = '<div>postfix</div>'
+    const expected = '<head></head><body><div>prefix</div>x<p>foo</p>y<div>postfix</div></body>'
 
-    expect(removePreambles(translate(input, 'the-utm-content'), false))
-      .to.include(expectedToInclude1)
-      .and.to.include(expectedToInclude2)
-      .and.to.include('utm_content=the-utm-content')
-      .and.to.include('Gil Tayar')
+    expect(translate(prefix, postfix, input)).to.equal(expected)
   })
 })
 
-function removePreambles(html, includingPrePostFixes = true) {
-  const pre = html.replace('<head></head><body>', '').replace('</body>', '')
-
-  if (!includingPrePostFixes) {
-    return pre
-  }
-
-  return pre.replace(/<!-- postfix -->(.|\s)*$/, '')
+function removePreambles(html) {
+  return html.replace('<head></head><body>', '').replace('</body>', '')
 }
